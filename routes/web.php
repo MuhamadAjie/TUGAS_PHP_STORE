0f36aeba4c04b47dcbacffc769a74c0330297d81
Muhamad Ajie Darmawan <muhamadajie97@gmail.com>
2021-07-10 16:55:00 +0700

    Upload Tugas PHP Online Store

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Utama;
+use App\Http\Controllers\Login;
+use App\Http\Controllers\Order;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', [Utama::class, 'index']);
+Route::post('/pushData', [Utama::class, 'store']);
+Route::get('/login', [Login::class, 'index']);
+Route::post('/daftar', [Login::class, 'register']);
+Route::post('/masuk', [Login::class, 'masuk']);
+Route::get('/keluar', [Login::class, 'keluar']);
+Route::post('/AddCart', [Order::class, 'order']);
+Route::get('/Keranjang', [Order::class, 'Keranjang']);
